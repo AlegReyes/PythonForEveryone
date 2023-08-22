df6147661ee58ce43939b2f347e0611306a03339
AleGR <alejandrogr9831@gmail.com>
2023-08-22 11:46:53 -0600

    agrego la carpeta con ejercicios de CodeWars

--- helloworld.py
+++ helloworld.py
@@ -1,16 +1,4 @@
 
- #Create a function with two arguments that will return an array of the first n multiples of x.
-    #Assume both the given number and the number of times to count will be positive numbers greater than 0.
-    #count_by(1,10) #should return [1,2,3,4,5,6,7,8,9,10]
-    #count_by(2,5) #should return [2,4,6,8,10]
-def count_by(a,b):
-    array=[]
-    for i in range(b):
-        array.append(a*(i+1))
-    return array
-result=count_by(3,10)
-print(result)
-
 """ Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. 
 It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
 Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical 
